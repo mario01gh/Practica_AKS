917aa48326a687de8b7a0bbc8fde451fbba24d68
mario01gh <63239192+mario01gh@users.noreply.github.com>
2023-03-04 22:13:42 +0100

    Add files via upload

--- AKS_SourceCode/Scratch.java
+++ AKS_SourceCode/Scratch.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.stream.DoubleStream;
 import java.io.File;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -19,7 +20,7 @@ public class Scratch {
 		//Lectura del txt con los números a procesar
 		// Se cargan todos los valores en un array de tamaño variable
 
-		File read_file = new File(System.getProperty("user.dir"), "primos_grandes.txt");
+		File read_file = new File(System.getProperty("user.dir"), "primes.txt");
 		String cadena;
 		List<BigInteger> number_array = new ArrayList<BigInteger>();
 
@@ -45,7 +46,7 @@ public class Scratch {
 			
 			//escritura en el archivo csv
 			try{
-				File write_file = new File(System.getProperty("user.dir"), "h1_digitos.csv");
+				File write_file = new File(System.getProperty("user.dir"), "h1_numeros.csv");
 				if (!write_file.exists()) {
 					write_file.createNewFile();
 				}
@@ -56,6 +57,56 @@ public class Scratch {
 			}catch (Exception e) {
 				e.printStackTrace();
 			}
-		}	
+		}
+
+//Codigo usado para generación de archivos de datos relacionados con el numero de digitos
+//--------------------------------------------------------------------------------
+// 		for (int digitos = 1; digitos < 20; digitos++){
+// 			//System.out.println(digitos);
+// 			ArrayList<Double> tiempo1 = new ArrayList<Double>();
+// 			ArrayList<Double> tiempo2 = new ArrayList<Double>();
+// 			ArrayList<Double> tiempo3 = new ArrayList<Double>();
+
+// 			int bits = (int) (digitos / Math.log10(2)); // Número de bits
+			
+// 			for(int i = 0; i < 5 ; i++){
+// 				SecureRandom random = new SecureRandom();
+// 				BigInteger n = BigInteger.probablePrime((int) bits, random);
+// 				//System.out.println(n);
+// 				AKS obj = new AKS(n);
+// 				double[] result = obj.isPrime();
+// 				tiempo1.add(result[0]);
+// 				tiempo2.add(result[1]);
+// 				tiempo3.add(result[2]);
+// 			}
+// 			double media1 = calcularMedia(tiempo1);
+// 			double media2 = calcularMedia(tiempo2);
+// 			double media3 = calcularMedia(tiempo3);
+			
+// 			String s = String.format("%f;%f;%f\n", media1/1000000, media2/1000000, media3/1000000);
+			
+// 			//escritura en el archivo csv
+// 			try{
+// 				File write_file = new File(System.getProperty("user.dir"), "h1_digitos_test.csv");
+// 				if (!write_file.exists()) {
+// 					write_file.createNewFile();
+// 				}
+// 				FileWriter fw = new FileWriter(write_file, true);
+// 				BufferedWriter bw = new BufferedWriter(fw);
+// 				bw.write(s);
+// 				bw.close();
+// 			}catch (Exception e) {
+// 				e.printStackTrace();
+// 			}
+// 		}
+//--------------------------------------------------------------------------------
+ 	}
+	public static double calcularMedia(ArrayList<Double> lista) {
+		// Calcular la suma de los elementos de la lista
+		double suma = 0;
+        for (double valor : lista) {
+            suma += valor;
+        }
+        return suma / lista.size();
 	}
 }
